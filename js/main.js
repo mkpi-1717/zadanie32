1c56955008008bc9d7a59db08402a55f9d83d1e3
mkpi-1717 <marekpisarczyk22@gmail.com>
2020-08-27 17:43:18 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var num1 = 2;
+var num2 = 5;
+
+
+function wieksza(num1, num2) {
+        if (num1 >= num2){
+         let wynik = num1;
+            } else {
+            wynik =num2;
+        }
+    return wynik;
+   }
+  
+    console.log(wieksza(num1, num2));
